b463cea872aac0dfaa0d81fd7ce63dc4dd3c85fe
Edwin Ko <edwin.mk.ko@gmail.com>
2023-10-23 11:42:16 +0000

    implemented middle.js

---
+++ middle.js
@@ -0,0 +1,44 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤡🤡🤡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const middle = function(arr) {
+  let middleElements = [];
+  
+  if (arr.length <= 2) {
+    return middleElements;
+  } else if (arr.length % 2 === 1) {
+    middleElements.push(arr[Math.floor(arr.length / 2)]);
+  } else {
+    middleElements.push(arr[(arr.length / 2) - 1]);
+    middleElements.push(arr[arr.length / 2]); 
+  }
+
+  return middleElements;
+};
+
+assertArraysEqual(middle([]), []);
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1,2]), []);
+assertArraysEqual(middle([1,2,3]), [2]);
+assertArraysEqual(middle([1,2,3,4,5,6,7]), [4]);
+assertArraysEqual(middle([1,2,3,4]), [2,3]);
+assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]);
