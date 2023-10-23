89d3a3d6e83cdbb04cc13ef5d7da7c7c811f2665
Edwin Ko <edwin.mk.ko@gmail.com>
2023-10-23 05:05:28 +0000

    implemented without.js

---
+++ without.js
@@ -0,0 +1,57 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤡🤡🤡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  let subset = [];
+  let isRemovable = false;
+
+  for (let i = 0; i < source.length; i++) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if(source[i] === itemsToRemove[j]) {
+        isRemovable = true;
+        break;
+      }
+    }
+    if (!isRemovable) {
+      subset.push(source[i]);
+    }
+
+    isRemovable = false;
+  }
+
+  return subset;
+};
+
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+console.log(without([], ))
+
+assertArraysEqual(without([1,2,3], [1]), [2,3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1","2"]);
+assertArraysEqual(without([], [1]), []);
+assertArraysEqual(without([], []), []);
+assertArraysEqual(without([1,2,3], []), [1,2,3]);
+assertArraysEqual(without([1,2,3], [1]), [2,3]);
+
+const words = [1, 2, 3];
+without(words, [1]);
+assertArraysEqual(words, [1,2,3]);
