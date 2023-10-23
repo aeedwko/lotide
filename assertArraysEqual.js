d7871a92cc660b19993941a37d1e5a16a266e830
Edwin Ko <edwin.mk.ko@gmail.com>
2023-10-23 04:13:35 +0000

    implemented assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,24 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤡🤡🤡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertArraysEqual(eqArrays([1,2,3], [1,2,3]), true);
+assertArraysEqual(eqArrays([1,2], [1,2,3]), false);
