50e416edbe71ca383880e44cead2bbf471df2fb3
Edwin Ko <edwin.mk.ko@gmail.com>
2023-10-23 04:16:14 +0000

    revised assertArraysEqual as per AI suggestions

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -20,5 +20,5 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-assertArraysEqual(eqArrays([1,2,3], [1,2,3]), true);
-assertArraysEqual(eqArrays([1,2], [1,2,3]), false);
+assertArraysEqual([1,2,3], [1,2,3]);
+assertArraysEqual([1,2], [1,2,3]);
