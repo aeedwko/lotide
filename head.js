c68aa8d656109d250fe0d8b6d662d131c0f9fa43
Edwin Ko <edwin.mk.ko@gmail.com>
2023-11-08 21:39:19 +0000

    refactored assertEqual.js and head.js

--- head.js
+++ head.js
@@ -1,19 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🤡🤡🤡 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const head = function(arr) {
   return arr[0];
 };
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1);
-assertEqual(head("Hello"), "H");
-assertEqual(head(["A", "E"]), "A");
-assertEqual(head([]), undefined);
+module.exports = head;
---
+++ test/headTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require('../assertEqual');
+const head = require('../head');
+
+// TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([1]), 1);
+assertEqual(head("Hello"), "H");
+assertEqual(head(["A", "E"]), "A");
+assertEqual(head([]), undefined);
