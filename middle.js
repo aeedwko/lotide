bef5b316de121c18a8eeed0e1c0d341d7c33eb58
Edwin Ko <edwin.mk.ko@gmail.com>
2023-11-09 22:52:32 +0000

    refactored middle.js and added test file

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🤡🤡🤡 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const middle = function(arr) {
   let middleElements = [];
   
@@ -35,10 +13,4 @@ const middle = function(arr) {
   return middleElements;
 };
 
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1,2]), []);
-assertArraysEqual(middle([1,2,3]), [2]);
-assertArraysEqual(middle([1,2,3,4,5,6,7]), [4]);
-assertArraysEqual(middle([1,2,3,4]), [2,3]);
-assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]);
+module.exports = middle;
