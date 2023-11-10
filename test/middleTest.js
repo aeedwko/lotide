136836d3e881425f39cdfa3d5926efb2f299cfbf
Edwin Ko <edwin.mk.ko@gmail.com>
2023-11-10 04:01:40 +0000

    corrected middleTest.js test cases as per AI suggestions

--- test/middleTest.js
+++ test/middleTest.js
@@ -14,19 +14,19 @@ describe("#middle", () => {
     assert.deepEqual(middle([1,2]), []);
   });
 
-  it("returns [] for middle([1,2,3])", () => {
+  it("returns [2] for middle([1,2,3])", () => {
     assert.deepEqual(middle([1,2,3]), [2]);
   });
 
-  it("returns [] for middle([1,2,3,4,5,6,7])", () => {
+  it("returns [4] for middle([1,2,3,4,5,6,7])", () => {
     assert.deepEqual(middle([1,2,3,4,5,6,7]), [4]);
   });
 
-  it("returns [] for middle([1,2,3,4])", () => {
+  it("returns [2,3] for middle([1,2,3,4])", () => {
     assert.deepEqual(middle([1,2,3,4]), [2,3]);
   });
 
-  it("returns [] for middle([1,2,3,4])", () => {
+  it("returns [3,4] for middle([1,2,3,4,5,6])", () => {
     assert.deepEqual(middle([1,2,3,4,5,6]), [3,4]);
   });
 });
