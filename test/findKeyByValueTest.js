9fc6e886e6c828a5858a60554730dfc759db79c8
Edwin Ko <edwin.mk.ko@gmail.com>
2023-11-12 10:13:19 +0000

    refactored findKeyByValueTest.js with chai

--- test/findKeyByValueTest.js
+++ test/findKeyByValueTest.js
@@ -1,4 +1,4 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const findKeyByValue = require('../findKeyByValue');
 
 const bestTVShowsByGenre = {
@@ -7,7 +7,17 @@ const bestTVShowsByGenre = {
   drama:  "The Wire"
 };
 
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sciFi");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+describe("#findKeyByValue", () => {
+  it("returns 'drama' for findKeyByValue(bestTVShowsByGenre, 'The Wire')", () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+  });
+  it("returns 'sciFi' for findKeyByValue(bestTVShowsByGenre, 'The Expanse')", () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sciFi");
+  });
+  it("returns 'comedy' for findKeyByValue(bestTVShowsByGenre, 'Brooklyn Nine-Nine')", () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+  });
+  it("returns undefined for findKeyByValue(bestTVShowsByGenre, `That '70s Show`)", () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+  });
+});
